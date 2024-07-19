6bb92e23f09a3904eb37187dad1bcddad9ce7c62
席万里 <2597029604@qq.com>
2024-07-19 12:25:44 +0800

    i

---
+++ goblog_web/vite.config.js
@@ -0,0 +1,42 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import {defineConfig, loadEnv} from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default ({mode})=>{
+  const env = loadEnv(mode,process.cwd())
+  const baseUrl = env.VITE_API
+  const baseUrlWs = env.VITE_WEBSOCKET
+  console.log(env)
+  return defineConfig({
+  envPrefix:["VITE_"],
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  server:{
+    host:"127.0.0.1",
+    port:5173,
+    proxy:{
+      "/uploads":{
+        target: baseUrl
+      },
+      "/wsUrl":{
+        target: baseUrlWs, //这里是后台ws访问地址
+        changeOrigin: true, //允许跨域设置
+        ws: true, //websocket代理设置
+        rewrite: (path)=> path.replace(/^\/wsUrl/,""), //拦截路径去除
+      },
+      "/api":{
+        target: baseUrl
+      }
+    }
+  }
+})
+}
+
+// https://vitejs.dev/config/
+// 重定向到代理地址
+
